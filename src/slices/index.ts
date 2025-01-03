b746c5050c31ae8b71306b4ddb3ca6e8886ee910
patgpt <doitformurphy@gmail.com>
2025-01-03 15:16:40 -0500

    Refactor layout structure by removing PrismicClientWrapper; delete unused AnimatedProfileImage and PrismicClientWrapper components

--- src/slices/index.ts
+++ src/slices/index.ts
@@ -4,11 +4,11 @@ import dynamic from "next/dynamic";
 
 export const components = {
   about_me_slice: dynamic(() => import("./AboutMeSlice")),
-  animated_profile_image: dynamic(() => import("./AnimatedProfileImage")),
+
   hero: dynamic(() => import("./Hero")),
   logo: dynamic(() => import("./Logo")),
   navigation_item: dynamic(() => import("./NavigationItem")),
-  parallax_image: dynamic(() => import("./ParallaxImage")),
+
   parallaxed_banner_with_title: dynamic(
     () => import("./ParallaxedBannerWithTitle"),
   ),
