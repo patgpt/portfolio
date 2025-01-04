f8798cf8bf9ff12446e390106bfc8ec4fda983dc
patgpt <doitformurphy@gmail.com>
2025-01-03 19:23:57 -0500

    fix: correct import casing in BentoGrid component

--- src/components/layout/BentoGrid.tsx
+++ src/components/layout/BentoGrid.tsx
@@ -1,4 +1,5 @@
-import { BentoBox } from "@/components/Layout/BentoBox";
+
+import { BentoBox } from "@/components/layout/BentoBox";
 import React from "react";
 import { v4 as uuidv4 } from "uuid"; // Import a proper UUID generator
 
