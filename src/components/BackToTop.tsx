88ee12a5b0080bd80a6edee2c7dd1a2bcd6a4b22
patgpt <doitformurphy@gmail.com>
2025-01-03 19:02:05 -0500

    fix build errors

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -2,7 +2,7 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { IoArrowUpCircleOutline } from "react-icons/io5";
 import { useEffect, useState } from "react";
-
+// TODO: Test
 export const BackToTop = () => {
   const [isVisible, setIsVisible] = useState(false);
 
